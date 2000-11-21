7bb4b559b01773a26803524344e5039b50026cad
igable <>
2000-11-21 02:23:34 +0000

    Changes for new branch of TDS

--- Event/TopLevel/EventModel.h
+++ Event/TopLevel/EventModel.h
@@ -1,4 +1,4 @@
-// $Id: EventModel.h,v 1.3 2000/10/30 20:28:26 heather Exp $
+// $Id: EventModel.h,v 1.4 2000/11/01 22:11:39 heather Exp $
 // 
 //  Original author: Sawyer Gillespie
 //                   hgillesp@u.washington.edu
@@ -19,12 +19,19 @@
     namespace EventModel {
         _EXTERN_ std::string   Event;
 
+
         namespace MC {
             _EXTERN_ std::string Event;
             _EXTERN_ std::string MCTKRHits;
             _EXTERN_ std::string MCCalorimeterHits;
             _EXTERN_ std::string MCACDHits;
         };
+
+        namespace Raw {
+            _EXTERN_ std::string Event;
+            _EXTERN_ std::string CsIDatas;
+        };
+
 
     };
 #undef _EXTERN_
