93bf1811713efacd03cd6a9f1981e075b055d04c
burnett <>
2002-04-02 18:35:07 +0000

    update to include access methods to all fields

--- Event/MonteCarlo/McParticle.h
+++ Event/MonteCarlo/McParticle.h
@@ -2,7 +2,7 @@
 * @file McReconAlg.cxx
 * @brief Declaration and definition of the TDS object McParticle.
 *
-*  $Header: /nfs/slac/g/glast/ground/cvs/GlastEvent/GlastEvent/MonteCarlo/McParticle.h,v 1.11 2002/03/27 05:33:21 burnett Exp $
+*  $Header: /nfs/slac/g/glast/ground/cvs/GlastEvent/GlastEvent/MonteCarlo/McParticle.h,v 1.12 2002/03/28 02:19:49 burnett Exp $
 */
 #ifndef GlastEvent_McParticle_H
 #define GlastEvent_McParticle_H 1
@@ -55,6 +55,7 @@ extern const CLID& CLID_McParticle;
 
 namespace mc {  // NameSpace
 
+
 class McParticle  : virtual public ContainedObject  {
   public:
     typedef int  StdHepId;
@@ -95,15 +96,13 @@ class McParticle  : virtual public ContainedObject  {
         const HepLorentzVector& finalMomentum,
         const HepPoint3D& finalPosition);
 
-    /// Retrieve particle identificatio
-    ParticleID particleID() const;
-    /// Update particle identification
-    void setParticleID( ParticleID value );
 
     /// Retrieve particle property
     StdHepId particleProperty() const;
-    /// Update particle identification
-    void setParticleProperty( StdHepId value );
+
+    /// retrieve all of status flags for const object
+    unsigned int statusFlags()const;
+
 
     /// Retrieve whether this is a primary particle: true if mother is itself
     bool primaryParticle() const;
@@ -113,6 +112,7 @@ class McParticle  : virtual public ContainedObject  {
     const HepPoint3D& finalPosition() const;
 
     const HepLorentzVector&  initialFourMomemtum()const;
+    const HepLorentzVector&  finalFourMomemtum()const;
 
     /// Serialize the object for writing
     virtual StreamBuffer& serialize( StreamBuffer& s ) const ;
--- src/MonteCarlo/McParticle.cxx
+++ src/MonteCarlo/McParticle.cxx
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/GlastEvent/src/MonteCarlo/McParticle.cxx,v 1.6 2002/03/26 21:35:20 burnett Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/GlastEvent/src/MonteCarlo/McParticle.cxx,v 1.7 2002/03/27 05:33:22 burnett Exp $
 
 #include <iostream>
 #include "GlastEvent/MonteCarlo/McParticle.h"
@@ -14,17 +14,10 @@ McParticle::StdHepId McParticle::particleProperty() const
 }
 
 
-/// Update particle property
-void McParticle::setParticleProperty( McParticle::StdHepId value )
-{
-  m_particleID = value;
-}
-
-
 /// Retrieve whether this is a primary particle
 bool McParticle::primaryParticle() const
 {
-  return m_statusFlags & PRIMARY;
+  return (m_statusFlags & PRIMARY)==PRIMARY;
 }
 
 void McParticle::init( McParticle* mother,         
@@ -54,6 +47,14 @@ const HepPoint3D& McParticle::finalPosition()const
 const HepLorentzVector&  McParticle::initialFourMomemtum()const
 {
     return m_initialFourMomentum;
+}
+const HepLorentzVector&  McParticle::finalFourMomemtum()const
+{
+    return m_finalFourMomentum;
+}
+
+unsigned int McParticle::statusFlags()const{
+    return m_statusFlags;
 }
 
 
