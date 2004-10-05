0ba6cfc6b78bfd4776c692909a898674fec41576
usher <>
2004-10-05 02:01:54 +0000

    Mods to build on linux

--- Event/Recon/TkrRecon/TkrTrackHit.h
+++ Event/Recon/TkrRecon/TkrTrackHit.h
@@ -26,7 +26,7 @@
 *
 * @author Bill Atwood, Leon Rochester, Johann Cohen, Tracy Usher
 *
-* $Header: /nfs/slac/g/glast/ground/cvs/Event/Event/Recon/TkrRecon/TkrTrackHit.h,v 1.1 2004/09/18 18:16:58 usher Exp $
+* $Header: /nfs/slac/g/glast/ground/cvs/Event/Event/Recon/TkrRecon/TkrTrackHit.h,v 1.2 2004/10/01 23:33:48 usher Exp $
 */
 
 static const CLID& CLID_TkrTrackHit = InterfaceID("TkrTrackHit",  1, 0);
@@ -68,14 +68,14 @@ public:
                     m_chiSquareFilter(0.), m_chiSquareSmooth(0.) {}
 
     /// Construct all but the track parameters, they must be set during recon stage
-    TkrTrackHit(TkrClusterPtr  cluster,
-                idents::TkrId& tkrID,
-                const double   hitZ,
-                const double   hitEnergy,
-                const double   hitRadLen,
-                const double   hitActDist,
-                const double   hitChiFilter,
-                const double   hitChiSmooth) :
+    TkrTrackHit(Event::TkrCluster* cluster,
+                idents::TkrId      tkrID,
+                const double       hitZ,
+                const double       hitEnergy,
+                const double       hitRadLen,
+                const double       hitActDist,
+                const double       hitChiFilter,
+                const double       hitChiSmooth) :
                     m_statusBits(0), m_cluster(cluster), m_hitID(tkrID), m_zPlane(hitZ), 
                     m_energy(hitEnergy), m_radLen(hitRadLen), m_activeDist(hitActDist),
                     m_chiSquareFilter(hitChiFilter), m_chiSquareSmooth(hitChiSmooth)
