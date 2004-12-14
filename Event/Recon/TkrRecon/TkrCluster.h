272879ce860baa3611b156d9c0a9caae95546ae0
lsrea <>
2004-12-14 02:34:49 +0000

    prepare for tag v9r13p1gr2 - Revised TkrCluster (on I&T branch)

--- Event/Recon/TkrRecon/TkrCluster.h
+++ Event/Recon/TkrRecon/TkrCluster.h
@@ -22,7 +22,7 @@
 *
 * @author Tracy Usher, Leon Rochester
 *
-* $Header: /nfs/slac/g/glast/ground/cvs/Event/Event/Recon/TkrRecon/TkrCluster.h,v 1.4 2003/02/27 06:58:01 lsrea Exp $
+* $Header: /nfs/slac/g/glast/ground/cvs/Event/Event/Recon/TkrRecon/TkrCluster.h,v 1.5 2004/03/11 05:15:13 heather Exp $
 */
 
 extern const CLID& CLID_TkrCluster;
@@ -30,99 +30,165 @@ extern const CLID& CLID_TkrCluster;
 namespace Event { //Namespace
 
     class TkrCluster : virtual public ContainedObject
-{
-public:
-    
-    /// enumeration of the view of the cluster
-    enum view 
     {
-        X, /**< cluster measures X */ 
+    public:
+
+        /// enumeration of the view of the cluster
+        enum view 
+        {
+            X, /**< cluster measures X */ 
             Y, /**< cluster measures Y */ 
             XY /**< not valid for clusters */
+        };
+
+        enum {Version=1};
+
+        enum {
+            fieldRawToT  = 0xff,  // for the raw ToT < 256
+            fieldFlag    = 1,     // "used" flag
+            fieldEnd     = 3,     // end for this hit 2 = mixed
+            fieldVersion = 3      // 0 for old style
+        };
+        enum {
+            shiftRawToT  = 8,     
+            shiftFlag    = 0,  // to preserve compatibility with old form
+            shiftEnd     = 1,
+            shiftVersion = 30
+        };
+        enum {
+            maskRawToT  = fieldRawToT<<shiftRawToT, 
+            maskFlag    = fieldFlag<<shiftFlag,
+            maskEnd     = fieldEnd<<shiftEnd,
+            maskVersion = fieldVersion<<shiftVersion
+        };
+
+        enum {NPLANES=18};
+
+        TkrCluster(){}
+        /// Constructor with arguments
+        /**
+        * Construct a cluster with all of its private members set
+        * @param id Sequential ID of cluster
+        * @param v  View of cluster (x=0, y=1)
+        * @param istrip0  first strip
+        * @param istripf  last strip
+        * @param ToT 
+        * @param tower tower number
+        */
+        TkrCluster(int id, int ilayer, int v, int istrip0, int istripf, 
+            Point position, double ToT, int tower, int rawToT, int end) 
+            : m_id(id), m_view(intToView(v)), m_plane(ilayer),
+            m_strip0(istrip0), m_stripf(istripf), m_position(position),
+            m_ToT(ToT), m_tower(tower), m_flag(Version<<shiftVersion) 
+        {
+            m_flag = m_flag | maskRawToT&(rawToT<<shiftRawToT);  
+            m_flag = m_flag | maskEnd&(end<<shiftEnd);
+        }
+         TkrCluster(int id, int ilayer, int v, int istrip0, int istripf, 
+            Point position, double ToT, int tower, int status) 
+            : m_id(id), m_view(intToView(v)), m_plane(ilayer),
+            m_strip0(istrip0), m_stripf(istripf), m_position(position),
+            m_ToT(ToT), m_tower(tower), m_flag(status)
+         {}
+
+       virtual ~TkrCluster() {}
+
+        //! Retrieve pointer to class defininition structure
+        virtual const CLID& clID() const   { return TkrCluster::classID(); }
+        static const CLID& classID()       { return CLID_TkrCluster; }
+
+        /// sets the flag of a cluster
+        inline void flag(int flag=1) {
+            if (flag==0) {
+                unflag();
+            } else {
+                m_flag = ( m_flag&maskVersion ? ((m_flag&~maskFlag) | (1<<shiftFlag)) : 1);
+            }
+        }
+        /// clears the flag of a cluster
+        inline void unflag() {
+            m_flag = ( m_flag&maskVersion ? m_flag&~maskFlag : 0);
+        }
+
+        inline int  tower()      const {return m_tower;}
+        inline int  id()         const {return m_id;}
+        inline int  plane()      const {return m_plane;}
+        inline view v()          const {return m_view;}
+        inline int  firstStrip() const {return m_strip0;}
+        inline int  lastStrip()  const {return m_stripf;}
+        inline double ToT()      const {return getRawToT();}
+
+        int chip()	             const {return m_strip0/64;}
+        double strip()           const {return 0.5*(m_strip0+m_stripf);}
+        double size ()           const {return std::abs(m_stripf-m_strip0) + 1.;}
+
+        Point position()         const {return m_position;}
+
+        /// tests if hit is used on a track
+        bool hitFlagged() const {
+            return ( m_flag&maskVersion ? ((m_flag&maskFlag)!=0) : (m_flag!=0) );
+        }
+
+        /// writes out the information of the cluster if msglevel is set to debug
+        void writeOut(MsgStream& log) const;
+
+        /// converts the view integer to enum view
+        static enum view intToView(int iv) {
+            TkrCluster::view v = XY;
+            if (iv == 0) v = X;
+            else if (iv == 1) v =Y;
+            return v;
+        }
+
+        /// converts enum to int; guarantees that X->0 and Y->1
+        static int viewToInt(view v)
+        {
+            if (v == TkrCluster::XY) return 2;
+            return (v == TkrCluster::X? 0:1);
+        }
+        /// retrieves raw ToT (will be raw or corrected depending on the version)
+        inline double getRawToT() const { 
+            return ( m_flag&maskVersion ? (m_flag&maskRawToT)>>shiftRawToT : m_ToT );
+        }
+        /// retrieve corrected ToT (zero for old-style records)
+        inline double getMips() const {
+            return ( m_flag&maskVersion ? m_ToT : 0.0 );
+        }
+        // retrieve full status word
+        inline int getStatusWord() const { return m_flag; }
+        /// retrieve end
+        inline int getEnd() const {
+            return ( m_flag&maskVersion ? m_flag&(maskEnd>>shiftEnd) : 2 );
+        }
+        /// retrieve version number (old-style is zero)
+        inline int getVersion() const {
+            return m_flag&maskVersion>>shiftVersion;
+        }
+
+    private:
+
+        /// tower id (should be idents/TowerId)
+        int m_tower;
+        /// not plane; actually recon layer number
+        int m_plane;
+        /// view (X or Y [XY is illegal here)
+        TkrCluster::view m_view;
+
+        /// initial strip address of the cluster
+        int m_strip0;
+        /// final strip address of the cluster
+        int m_stripf;
+        /// ToT value, raw or corrected, depends on version
+        double m_ToT;
+        /// space position of the cluster
+        Point m_position;
+
+        /// id of the cluster, sequential in order of construction
+        int m_id;
+        /// used flag, or status word, depends on version
+        int m_flag;
+
     };
-    enum {NPLANES=18};
-    
-    friend class TkrClusters;
-    
-public:
-    
-    TkrCluster(){}
-    /// Constructor with arguments
-    /**
-    * Construct a cluster with all of its private members set
-    * @param id Sequential ID of cluster
-    * @param v  View of cluster (x=0, y=1)
-    * @param istrip0  first strip
-    * @param istripf  last strip
-    * @param ToT 
-    * @param tower tower number
-    */
-    TkrCluster(int id, int ilayer, int v, 
-        int istrip0, int istripf, Point position, double ToT, int tower = 0);
-    virtual ~TkrCluster() {}
-
-    //! Retrieve pointer to class defininition structure
-   virtual const CLID& clID() const   { return TkrCluster::classID(); }
-   static const CLID& classID()       { return CLID_TkrCluster; }
-
-
-    
-    // set methods
-    /// sets the flag of a cluster
-	inline void flag(int flag=1) {m_flag = flag;}
-	/// clears the flag of a cluster
-    inline void unflag()         {m_flag = 0;}
-	
-    // get methods
-	inline int  tower()      const {return m_tower;}
-    inline int  id()         const {return m_id;}
-    inline int  plane()      const {return m_plane;}
-    inline view v()          const {return m_view;}
-    inline int  firstStrip() const {return m_strip0;}
-    inline int  lastStrip()  const {return m_stripf;}
-    inline double ToT()      const {return m_ToT;}
-
-	int chip();
-	double strip();
-	double size ();
-    
-    Point position()       const {return m_position;}
-    
-    /// returns true if the cluster has been flagged
-    bool hitFlagged()      const {return (m_flag!=0);}
-    
-    /// writes out the information of the cluster if msglevel is set to debug
-    void writeOut(MsgStream& log) const;
-    
-    /// converts the view integer to enum view
-    static enum view intToView(int);
-    /// converts enum to int; guarantees that X->0 and Y->1
-    static int viewToInt(view v);
-    
-private:
-    
-    /// tower id (should be idents/TowerId)
-    int m_tower;
-    /// plane id: this is the bi-layer number, but ordered backwards (0 at front)
-    int m_plane;
-    /// view (X or Y [XY is illegal here)
-    TkrCluster::view m_view;
-    
-    /// initial strip address of the cluster
-    int m_strip0;
-    /// final strip address of the cluster
-    int m_stripf;
-    /// ToT value of the cluster
-    double m_ToT;
-    /// space position of the cluster
-    Point m_position;
-    
-    /// id of the cluster, sequential in order of construction
-    int m_id;
-    /// flag of the cluster, used during pattern recognition
-    int m_flag;
-    
-};
 
 }; //Namespace
 
--- src/Recon/TkrRecon/TkrCluster.cxx
+++ src/Recon/TkrRecon/TkrCluster.cxx
@@ -6,40 +6,7 @@
 
 using namespace Event;
 
-TkrCluster::TkrCluster(int id, int ilayer, int v,
-                       int istrip0, int istripf, Point position, double ToT, int tower)
-					   
-{
-	// Purpose and method: makes a cluster with attributes
-	// Input:  id is the sequential cluster number
-	//         v  is the measured view (0->X, 1->Y)
-	//         ilayer is the bilayer number (0 at the front)
-	//         istrip0 is the first strip in the cluster
-	//         istripf is the last strip in the cluster
-	//         ToT is the time-over-threshold for this cluster
-	//         tower is the tower number
-	// Output: a cluster
-	// Dependencies: none
-	// Caveats: none
-	
-    m_id     = id;
-	m_view   = intToView(v);
-	
-	m_plane  = ilayer;
-	
-	m_strip0 = istrip0;
-	m_stripf = istripf;
-	
-	m_position = position;
-	
-	m_ToT    = ToT;
-    m_tower  = tower;
-	
-    m_flag   = false;
-}
-
 void TkrCluster::writeOut(MsgStream& log) const
-
 {
     // Purpose: writes out debug info
 	// Inputs:  message stream
@@ -47,54 +14,10 @@ void TkrCluster::writeOut(MsgStream& log) const
 	
     log << MSG::DEBUG;
     if (log.isActive()) {
-        log << " plane " << m_plane << " XY " << (int) m_view
+        log << " bilayer " << m_plane << " XY " << (int) m_view
             << " xpos  " << m_position.x()  << " ypos   " << m_position.y()
             << " zpos  " << m_position.z()
             << " i0-if " << m_strip0 <<"-"<< m_stripf;
     }
     log << endreq;
-}
-//---------  Private --------------------------------
-
-
-int TkrCluster::chip()     
-{ 
-	// Purpose: returns chip number of this cluster
-	// Method: calculated from the strip number
-	return m_strip0/64;
-}
-
-double TkrCluster::strip() 
-{ 
-	// Purpose: return the center strip number
-	// Method: average of first and last strip
-	// Caveat: may be half-integer
-	return 0.5*(m_strip0+m_stripf);
-}
-
-double TkrCluster::size()  
-{ 
-	// Purpose: returns number of strips in the cluster
-    return std::abs(m_stripf-m_strip0) + 1.;
-}
-
-TkrCluster::view TkrCluster::intToView(int iv)
-
-{
-	// Purpose: converts a view number to an enum
-	
-	TkrCluster::view v = XY;
-	if (iv == 0) v = X;
-	else if (iv == 1) v =Y;
-	return v;
-}
-
-
-int TkrCluster::viewToInt(TkrCluster::view v)
-
-{
-	// Purpose: converts an enum to an integer
-	
-	if (v == TkrCluster::XY) return 2;
-	return (v == TkrCluster::X? 0:1);
 }
