b35e3eec3620dff19635752995f4cfd3ff7fc2c6
chamont <>
2005-12-02 16:00:33 +0000

    doc

--- Event/Recon/AcdRecon/AcdRecon.h
+++ Event/Recon/AcdRecon/AcdRecon.h
@@ -34,7 +34,7 @@ static const CLID& CLID_AcdRecon = InterfaceID("AcdRecon", 2, 0);
 
 *                                 
 * @author Heather Kelly
-* $Header: /nfs/slac/g/glast/ground/cvs/Event/Event/Recon/AcdRecon/AcdRecon.h,v 1.11.4.4 2005/11/03 19:40:44 echarles Exp $          
+* $Header: /nfs/slac/g/glast/ground/cvs/Event/Event/Recon/AcdRecon/AcdRecon.h,v 1.13 2005/11/09 01:11:09 heather Exp $          
 */
 
 namespace Event {
@@ -83,6 +83,9 @@ namespace Event {
             m_rowActDist3DCol.resize(4, -2000.0);
         };
 
+        // DC: this was, as compared to the previous one,
+        // has two additionnal ribbon* arguments, and
+        // three 
         AcdRecon(double e, double ribbonE, int count, int ribbonCount, 
             double gDoca, double doca, 
             const idents::AcdId &minDocaId, double actDist,
@@ -91,10 +94,11 @@ namespace Event {
             const std::vector<double> &rowActDist,
             const std::vector<idents::AcdId>& idCol, 
             const std::vector<double>& energyCol,
-            double ribbon_actDist, const idents::AcdId ribbon_actDist_id,
-	    const std::vector<AcdTkrIntersection*>& acdTkrIntersections,
-	    double actDist3D, const idents::AcdId &maxActDist3DId, 
-	    const std::vector<double> &rowActDist3D, double cornerDoca)
+            double ribbon_actDist, const idents::AcdId ribbon_actDist_id, // additionnal
+            const std::vector<AcdTkrIntersection*>& acdTkrIntersections,
+            double actDist3D, const idents::AcdId &maxActDist3DId,        // additionnal
+            const std::vector<double> &rowActDist3D,                      // additionnal
+            double cornerDoca)
             : m_totEnergy(e),
             m_totRibbonEnergy(ribbonE),
             m_tileCount(count),
