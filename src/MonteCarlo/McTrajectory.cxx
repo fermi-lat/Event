be914d121e74f93c6f96042ca3996d67f4bc99c2
usher <>
2007-02-15 19:17:16 +0000

    Changes to incorporate the output of McTrajectories in the root output files. Also, update relational tables to fix a bug with removing elements, change also removes need for related items to inherit from DataObject.

--- src/MonteCarlo/McTrajectory.cxx
+++ src/MonteCarlo/McTrajectory.cxx
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/Event/src/MonteCarlo/McTrajectory.cxx,v 1.1 2002/07/18 08:33:23 riccardo Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/Event/src/MonteCarlo/McTrajectory.cxx,v 1.2 2006/03/21 01:29:45 usher Exp $
 
 #include <iostream>
 #include <math.h>
@@ -9,30 +9,42 @@
 namespace Event{
 
 /// Retrieve pointer to McParticle (const or non-const)
-const McParticle* McTrajectory::getMcParticle() const
-{
-    return m_mcParticle; 
-}
-McParticle* McTrajectory::getMcParticle()
-{
-    return m_mcParticle; 
-}
+//const McParticle* McTrajectory::getMcParticle() const
+//{
+//    return m_mcParticle; 
+//}
+//McParticle* McTrajectory::getMcParticle()
+//{
+//    return m_mcParticle; 
+//}
 
+McTrajectory::~McTrajectory()
+{
+    for(std::vector<McTrajectoryPoint*>::iterator trajIter = m_points.begin(); trajIter != m_points.end(); trajIter++)
+    {
+        delete *trajIter;
+    }
+}
 
 /// Update pointer to McParticle (by a C++ pointer or a smart reference)
-void McTrajectory::setMcParticle( McParticle* value )
-{
-    m_mcParticle = value; 
-}
-void McTrajectory::setMcParticle( SmartRef<McParticle> value )
-{ 
-    m_mcParticle = value; 
-}
+//void McTrajectory::setMcParticle( McParticle* value )
+//{
+//    m_mcParticle = value; 
+//}
+//void McTrajectory::setMcParticle( SmartRef<McParticle> value )
+//{ 
+//    m_mcParticle = value; 
+//}
 
 
-void McTrajectory::addPoints(std::vector<CLHEP::Hep3Vector>& points)
+void McTrajectory::addPoints(std::vector<Event::McTrajectoryPoint*>& points)
 {
   m_points = points;
+}
+
+void McTrajectory::addPoint(Event::McTrajectoryPoint* point)
+{
+    m_points.push_back(point);
 }
 
 }
