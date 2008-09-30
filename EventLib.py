a34eb88fee5b179ae209e77c03c83963fe9f76c7
ecephas <>
2008-09-30 17:11:58 +0000

    Added gaudi and clhep libraries via:
    env.Tool('addLibrary', library = env['clhepLibs'])
    env.Tool('addLibrary', library = env['gaudiLibs'])

--- EventLib.py
+++ EventLib.py
@@ -1,9 +1,10 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/Event/EventLib.py,v 1.1 2008/07/09 21:13:41 glastrm Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['Event'])
     env.Tool('geometryLib')
     env.Tool('identsLib')
-
+    env.Tool('addLibrary', library = env['clhepLibs'])
+    env.Tool('addLibrary', library = env['gaudiLibs'])
 def exists(env):
     return 1;
