9b9ca6c1cad5f5350e2ddce3e717a8d1e66e62a3
jrb <>
2012-08-17 00:03:43 +0000

    for Windows no-installed-headers

--- EventLib.py
+++ EventLib.py
@@ -1,10 +1,14 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/Event/EventLib.py,v 1.1 2008/07/09 21:13:41 glastrm Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/Event/EventLib.py,v 1.2 2008/09/30 17:11:57 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['Event'])
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'Event') 
     env.Tool('geometryLib')
     env.Tool('identsLib')
     env.Tool('addLibrary', library = env['clhepLibs'])
     env.Tool('addLibrary', library = env['gaudiLibs'])
+    if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+        env.Tool('findPkgPath', package = 'enums')
 def exists(env):
     return 1;
